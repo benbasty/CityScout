f368ae15d200fbf8013e9e9bf84de02a48176cf2
benbasty <benbasty@yahoo.fr>
2025-03-17 13:04:38 +0800

    Refactor file input handling and styles for improved image preview functionality; remove unused custom file input script

--- public/javascripts/addFileName.js
+++ public/javascripts/addFileName.js
@@ -1,6 +1,6 @@
 function previewMultiple(event) {
-    // const form = document.querySelector('#formFile');
-    // form.innerHTML = "";
+    const form = document.querySelector('#formFile');
+    form.innerHTML = "";
     var images = document.getElementById("image");
     var number = images.files.length;
     for (i = 0; i < number; i++) {
