415d99f17fe1206af26fa1ea624865f8f8331b3b
benbasty <benbasty@yahoo.fr>
2025-03-25 08:28:05 +0800

    Integrate Cloudinary for dynamic image sourcing in business seeding; update image display logic in business index view to handle missing images gracefully

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,14 @@ const cities = require('./cities');
 const { descriptors, places } = require('./seedHelpers');
 
 const Business = require('../models/businesses');
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 
@@ -19,25 +27,35 @@ const seedDB = async () => {
     await Business.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random = Math.floor(Math.random() * cities.length);
-        const price = Math.floor(Math.random() * 50) + 50
+        const price = Math.floor(Math.random() * 50) + 50;
+        const { resources } = await cloudinary.search
+        .expression('folder:CityScout')
+        .execute();
+        const randomIndex = Math.floor(Math.random() * resources.length);
+        const randomImage = resources[randomIndex];
+        const randomImageUrl = randomImage.secure_url;
+        const randomImageId = randomImage.public_id;
+
         const business = new Business({
             author: '67c52f0cd178ef7ef49c22b0',
             city: cities[random].city,
             state: cities[random].state,
             title: `${sample(descriptors)} ${sample(places)}`,
-            // images: `https://picsum.photos/400?random=${Math.random()}`,
             images: [
                 {
-                    url: 'https://res.cloudinary.com/dq2to8sns/image/upload/v1741983809/CityScout/wjgz1ljtp7gdljsnuath.jpg',
-                    filename: 'CityScout/wjgz1ljtp7gdljsnuath'
-                },
-                {
-                    url: 'https://res.cloudinary.com/dq2to8sns/image/upload/v1741983808/CityScout/rccnckr1idrvtjserct4.jpg',
-                    filename: 'CityScout/rccnckr1idrvtjserct4'
+                    url: randomImageUrl,
+                    filename: randomImageId
                 }
             ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, atque.',
-            price
+            price,
+            geometry: {
+                type: 'Point',
+                coordinates: [
+                    cities[random].longitude,
+                    cities[random].latitude
+                ]
+            }
         });
         await business.save();
         console.log(business);
