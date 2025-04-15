7a2817152a92058e561e5db328244e320f5fad7b
benbasty <benbasty@yahoo.fr>
2025-04-15 17:04:32 +0800

    Update MongoDB connection to use environment variable and add .gitignore for sensitive files

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const usersRoutes = require('./routes/users');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-// const dbUrl = process.env.DB_URL;
-const dbUrl = 'mongodb://127.0.0.1:27017/business';
+const dbUrl = process.env.DB_URL;
+// const dbUrl = 'mongodb://127.0.0.1:27017/business';
 const MongoStore = require('connect-mongo');
 
 
@@ -34,7 +34,8 @@ const MongoStore = require('connect-mongo');
 main().catch(err => console.log(err));
 //'mongodb://127.0.0.1:27017/business'
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/business');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/business');
+    await mongoose.connect(dbUrl);
 }
 
 const app = express();
